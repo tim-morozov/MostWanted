d170370db18dfa78d64697d2eddeb395688842a9
tim-morozov <tim.morozov@gmail.com>
2020-05-29 09:56:31 -0500

    created a searchByTraits function and added it to app function

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ function app(people){
       break;
     case 'no':
       // TODO: search by traits
+      searchResults = searchByTraits(people);
       break;
       default:
     app(people); // restart app
@@ -69,6 +70,10 @@ function searchByName(people){
   })
   // TODO: find the person using the name they entered
   return foundPerson;
+}
+
+function searchByTraits(people){
+
 }
 
 // alerts a list of people
